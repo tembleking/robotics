21bf724f3ae4e27c5116877a3485979c25095ede
Javier Fuster <fusti92@gmail.com>
2022-03-03 17:55:42 +0100

    feat: implement set speed

---
+++ robotics/robot/robot.py
@@ -0,0 +1,35 @@
+import numpy as np
+class Robot:
+    def __init__(self,
+                    wheel_radius,
+                    axis_length, 
+        claw_motor=None, 
+        left_motor=None, 
+        right_motor=None):
+        self.wheel_radius = wheel_radius
+        self.axis_length = axis_length
+        self.direct_control_matrix = np.matrix(
+            [
+                [self.wheel_radius/2, self.wheel_radius/2],
+                [self.wheel_radius/self.axis_length, -self.wheel_radius/self.axis_length]
+            ])
+        self.inverse_control_matrix = np.linalg.inv(self.direct_control_matrix)
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+        self.claw_motor = claw_motor
+
+
+    def set_speed(self, v, w):
+        angular_speed = np.dot(self.inverse_control_matrix,np.array([v,w]))
+
+        self.left_motor.set_speed(angular_speed[0,1])
+        self.right_motor.set_speed(angular_speed[0,0])
+
+    def read_speed(self):
+        pass
+    
+    def close_claw(self):
+        pass
+
+    def open_claw(self):
+        pass
---
+++ specs/robot_spec.py
@@ -0,0 +1,43 @@
+from mamba import describe, context, it
+from doublex import Spy, assert_that, called
+from robotics.robot.robot import Robot
+
+with describe("robot") as self:
+    with before.each:
+        self.left_motor = Spy()
+        self.right_motor = Spy()
+        self.robot = Robot(
+            left_motor=self.left_motor,
+            right_motor=self.right_motor,
+            wheel_radius=1,
+            axis_length=1
+        )
+    with context("we tell the robot to go at a certain speed"):
+        with it("stops the robot"):
+            self.robot.set_speed(0, 0)
+
+            assert_that(self.left_motor.set_speed, called().with_args(0))
+            assert_that(self.right_motor.set_speed, called().with_args(0))
+
+        with it("goes in a straight line"):
+            self.robot.set_speed(1, 0)
+
+            assert_that(self.left_motor.set_speed, called().with_args(1))
+            assert_that(self.right_motor.set_speed, called().with_args(1))
+
+        with it("turns left"):
+            self.robot.set_speed(0, 1)
+
+            assert_that(self.left_motor.set_speed, called().with_args(-0.5))
+            assert_that(self.right_motor.set_speed, called().with_args(0.5))
+        with it("turns right"):
+            self.robot.set_speed(0, -1)
+
+            assert_that(self.left_motor.set_speed, called().with_args(0.5))
+            assert_that(self.right_motor.set_speed, called().with_args(-0.5))
+        with it("turns while advancing"):
+            self.robot.set_speed(1, 1)
+
+            assert_that(self.left_motor.set_speed, called().with_args(0.5))
+            assert_that(self.right_motor.set_speed, called().with_args(1.5))
+            
