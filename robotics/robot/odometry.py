07c630fc6c87430db44eedeadbe44e691122e4d2
Federico Barcelona Auria <fede_rico_94@hotmail.com>
2022-03-17 18:26:58 +0100

    feat: Implement the read speed in odometry

--- robotics/robot/odometry.py
+++ robotics/robot/odometry.py
@@ -1,9 +1,45 @@
+import time
+
 from robotics.geometry import Location
+import numpy as np
 
 
 class Odometry:
-    def __init__(self):
+    def __init__(self, left_motor, right_motor, polling_period, wheel_radius, axis_length):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+        self.polling_period = polling_period
+        self.wheel_radius = wheel_radius
+        self.axis_length = axis_length
+
+    def start(self):
         pass
+
+    def read_speed(self):
+        left_angle = self.left_motor.get_last_angle()
+        left_speed = left_angle / self.polling_period
+
+        right_angle = self.right_motor.get_last_angle()
+        right_speed = right_angle / self.polling_period
+
+        transformation_matrix = np.matrix([[self.wheel_radius / 2, self.wheel_radius / 2],
+                                           [self.wheel_radius / self.axis_length,
+                                            -self.wheel_radius / self.axis_length]])
+
+        result = transformation_matrix.dot(np.matrix([[right_speed], [left_speed]]))
+        return result[0, 0], result[1, 0]
+
+    def _update(self):
+        while not self.finished.value:
+            # current processor time in a floating point value, in seconds
+            initial_time = time.clock()
+
+            # Read from the motors
+            v, w = self.read_speed()
+            # Update the Location
+
+            end_time = time.clock()
+            time.sleep(self.polling_period - (end_time - initial_time))
 
     def location(self) -> Location:
         pass
--- specs/odometry_spec.py
+++ specs/odometry_spec.py
@@ -1,22 +1,61 @@
-import time
 import math
 from unittest.mock import MagicMock
 
+from mamba import description, context, it
 from hamcrest import assert_that, is_, close_to
 
-from robotics.geometry import Point, Direction, Location
+from robotics.robot.odometry import Odometry
 
 with description('Odometry', 'unit') as self:
-    with context ('when it receives the angles from the wheels'):
-    	with it('tells us the odometry speed'):
-    		left_motor = MagicMock()
-    		left_motor.get_last_angle.return_value = math.radians(5)
-    		right_motor = MagicMock()
-    		right_motor.get_last_angle.return_value = math.radians(5)
-    		odometry = Odometry(left_motor, right_motor, polling_period = 0.01)
-    		odometry.start()
+    with context('when it receives the angles from the wheels'):
+        with it('tells us the odometry speed when going straight'):
+            left_motor = MagicMock()
+            left_motor.get_last_angle.return_value = math.radians(5)
+            right_motor = MagicMock()
+            right_motor.get_last_angle.return_value = math.radians(5)
 
-    		time.sleep(1)
-	        v, w = odometry.read_speed()
-	        assert_that(v, is_(close_to(46.22, 0.01)))
-	        assert_that(w, is_(0))
+            odometry = Odometry(left_motor, right_motor, polling_period=0.01,
+                                wheel_radius=0.0265, axis_length=0.119)
+
+            v, w = odometry.read_speed()
+            assert_that(v, is_(close_to(0.2312, 0.0001)))
+            assert_that(w, is_(0))
+
+        with it('tells us the odometry speed for a turn to the right'):
+            left_motor = MagicMock()
+            left_motor.get_last_angle.return_value = math.radians(5)
+            right_motor = MagicMock()
+            right_motor.get_last_angle.return_value = math.radians(-5)
+
+            odometry = Odometry(left_motor, right_motor, polling_period=0.01,
+                                wheel_radius=0.0265, axis_length=0.119)
+
+            v, w = odometry.read_speed()
+            assert_that(v, is_(0))
+            assert_that(w, is_(close_to(-3.8866, 0.0001)))
+
+        with it('tells us the odometry speed for a turn to the left'):
+            left_motor = MagicMock()
+            left_motor.get_last_angle.return_value = math.radians(-5)
+            right_motor = MagicMock()
+            right_motor.get_last_angle.return_value = math.radians(5)
+
+            odometry = Odometry(left_motor, right_motor, polling_period=0.01,
+                                wheel_radius=0.0265, axis_length=0.119)
+
+            v, w = odometry.read_speed()
+            assert_that(v, is_(0))
+            assert_that(w, is_(close_to(3.8866, 0.0001)))
+
+        with it('tells us the odometry speed for an arc'):
+            left_motor = MagicMock()
+            left_motor.get_last_angle.return_value = math.radians(2)
+            right_motor = MagicMock()
+            right_motor.get_last_angle.return_value = math.radians(5)
+
+            odometry = Odometry(left_motor, right_motor, polling_period=0.01,
+                                wheel_radius=0.0265, axis_length=0.119)
+
+            v, w = odometry.read_speed()
+            assert_that(v, is_(close_to(0.1618, 0.0001)))
+            assert_that(w, is_(close_to(1.1659, 0.0001)))
