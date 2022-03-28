490f6fc929d55484ddffcd6557e7b68726a36484
Javier Fuster <fusti92@gmail.com>
2022-03-28 10:02:07 +0200

    fix: broken tests due to use of meters

--- specs/controller_spec.py
+++ specs/controller_spec.py
@@ -29,7 +29,7 @@ with description('controller', 'unit') as self:
                                      trajectory_generator=trajectory_generator)
         self.controller.start()
 
-        robot.set_speed.assert_called_with(15, 0)
+        robot.set_speed.assert_called_with(0.15, 0)
         assert_that(robot.set_speed.call_count, is_(5))
 
     with it('turns around'):
@@ -54,19 +54,19 @@ with description('controller', 'unit') as self:
     with it('describes an arc to arrive to the destination'):
         odometry = MagicMock()
         odometry.location.side_effect = [
-            Location.from_angle_degrees(Point(40, 0), 90),
-            Location.from_angle_degrees(Point(math.sqrt(2) * 40 / 2, math.sqrt(2) * 40 / 2), 135),
-            Location.from_angle_degrees(Point(0, 40), 180),
+            Location.from_angle_degrees(Point(.40, 0), 90),
+            Location.from_angle_degrees(Point(math.sqrt(2) * 0.40 / 2, math.sqrt(2) * 0.40 / 2), 135),
+            Location.from_angle_degrees(Point(0, .40), 180),
         ]
         robot = MagicMock()
         robot.set_speed = MagicMock()
 
-        trajectory_generator = TrajectoryGenerator([Location.from_angle_degrees(Point(0, 40), 180)])
+        trajectory_generator = TrajectoryGenerator([Location.from_angle_degrees(Point(0, 0.40), 180)])
         self.controller = Controller(odometry=odometry, robot=robot, polling_period=0.2,
                                      trajectory_generator=trajectory_generator)
         self.controller.start()
 
-        robot.set_speed.assert_called_with(15, 15 / 40)
+        robot.set_speed.assert_called_with(0.15, 15 / 40)
         assert_that(robot.set_speed.call_count, is_(2))
 
     with it('stores the locations of the visited points'):
