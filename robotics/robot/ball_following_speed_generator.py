a86d5cc5534fe54fecc914c5131f4d5af0aee64d
Javier Fuster <fusti92@gmail.com>
2022-04-06 18:00:00 +0200

    feat: implement robot follows ball

--- robotics/robot/ball_following_speed_generator.py
+++ robotics/robot/ball_following_speed_generator.py
@@ -12,9 +12,13 @@ class BallFollowingSpeedGenerator:
         self.distance_goal = distance_goal
         self.distance_damping = distance_damping
         self.area_damping = area_damping
+        self.last_ball_position = 1
 
     def next_speed(self) -> Tuple[float, float]:
         ball_center, ball_size = self.camera.get_blob_position_and_size()
+        if ball_center is None:
+            return 0, 0.5 * self.last_ball_position
+        print('Ball center: %s, Ball size: %s' % (ball_center, ball_size))
         v = self._calculate_lineal_speed(ball_size)
         w = self._calculate_angular_speed(ball_center)
         return v, w
@@ -23,4 +27,10 @@ class BallFollowingSpeedGenerator:
         return self.area_damping * (self.area_goal - ball_size)
 
     def _calculate_angular_speed(self, ball_center: Point) -> float:
-        return self.distance_damping * (self.distance_goal - ball_center.x)
+        offset = self.distance_goal - ball_center.x
+        if offset < 0:
+            self.last_ball_position = -1
+        else:
+            self.last_ball_position = 1
+
+        return self.distance_damping * offset
